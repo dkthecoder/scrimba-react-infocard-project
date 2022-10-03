da26f036d864fd968f498e9a4e9abd72e99375b9
Dee Kay <dk@email.com>
2022-10-04 00:34:50 +0200

    finished items

--- src/App.js
+++ src/App.js
@@ -4,37 +4,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
+      <header className="App-body">
 
         <body>
 
           <main class="w-100 m-auto">
-            <form>
-              <img src={profile} className="App-logo" alt="profile" />
-              <h1>Dee Kay</h1>
-              <h3>Fullstack/Frontend Developer</h3>
+            <img src={profile} className="App-logo" alt="profile" />
+            <h1>Dee Kay</h1>
+            <h3>Fullstack/Frontend Dev</h3>
 
-              <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                <button type="button" class="btn btn-primary px-4">📧 email</button>
-                <button type="button" class="btn btn-outline-primary px-4">🕴️ linkedin</button>
-              </div>
+            <div class="d-grid gap-3 d-sm-flex justify-content-sm-center">
+              <button type="button" class="btn btn-outline-dark px-4">📧 email</button>
+              <button type="button" class="btn btn-outline-dark px-4">🕴️ linkedin</button>
+            </div>
 
+            <div>
+              <h5>About</h5>
+              <p>I love to code and code to love more... Getting into fullstack was the starting point but i love design and creating visually appeasing interfaces. So frontend is for me, what butter is to toast.</p>
+              <h5>Interests</h5>
+              <p>coding, coding, blokchain, coding, muay thai, coding, travel, coding...</p>
+            </div>
 
-
-              <div class="form-floating gap">
-                <h5>About</h5>
-                <p>I love to code and code to love more... Getting into fullstack was the starting point but i love design and creating visually appeasing interfaces. So frontend is for me, what butter is to toast.</p>
-              </div>
-              <div class="form-floating">
-                <h5>Interests</h5>
-                <p>coding, coding, blokchain, coding, muay thai, coding, travel, coding...</p>
-              </div>
-            </form>
           </main>
-
-
-
         </body>
       </header>
 
